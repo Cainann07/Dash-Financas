3b72ca6ab0a66c596776a4d71d8f8480f1b69b93
Cainann07 <mateuscainan14@gmail.com>
2025-12-09 00:01:20 -0300

    :lipstick: Melhorias nos estilos.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule, Button } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, InputTextModule, Button, PasswordModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+    email_input: string | undefined;
+    user_input: string | undefined;
+    senha_input: string | undefined;
 
 }
