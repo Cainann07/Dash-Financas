192e273033245b325fd5457734823d1cf3c1f2ec
Cainann07 <mateuscainan14@gmail.com>
2025-12-29 16:05:31 -0300

    Estruturação do back-end para desenvolvimento.

---
+++ back-end/src/main/java/com/mateuss/financeiro_api/FinanceiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.mateuss.financeiro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroApiApplication.class, args);
+	}
+
+}
